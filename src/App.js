ebb95cbbd513aa6772ded4eeabc63b3b742c42e7
wenkuan Xiao <93316790+LazyDreamingDog@users.noreply.github.com>
2022-06-25 16:49:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <span><a href="http://139.159.141.28:8080/createWeId" target="_blank" rel="noopener noreferrer">进入系统</a></span>
+      <span><a href="http://119.3.139.4:80" target="_blank" rel="noopener noreferrer">进入系统</a></span>
         <></>
-        <span><a href="http://139.159.141.28:8080/createWeId">进入系统</a></span>
+        <span><a href="http://119.3.139.4:80">进入系统</a></span>
     </div>
   );
 }
